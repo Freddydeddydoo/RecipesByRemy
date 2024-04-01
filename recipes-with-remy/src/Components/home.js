5815453f87975f8af1e09d3325fd327150d20a42
fardin.mahid <fardin.mahid@ucalgary.ca>
2024-04-01 09:12:36 -0600

    'my intial changes'

---
+++ recipes-with-remy/src/Components/home.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Home = (props) => {
+    const { loggedIn, email } = props
+    const navigate = useNavigate()
+
+    const onButtonClick = () => {
+        // You'll update this function later
+    }
+
+    return (
+    <div className="mainContainer">
+
+        <div className={'titleContainer'}>
+            <div>Welcome!</div>
+        </div>
+        <div>This is the home page.</div>
+        <div className={'buttonContainer'}>
+            <input className={'inputButtonWhite'} type="button" onClick={onButtonClick} value={'Sign up'}/>
+            {loggedIn ? <div>Your email address is {email}</div> : <div />}
+        </div>
+        <div className={'buttonContainer'}>
+            <input className={'inputButtonWhite'} type="button" onClick={onButtonClick} value={'Log in'}/>
+            {loggedIn ? <div>Your email address is {email}</div> : <div />}
+        </div>
+        
+    </div>
+    )
+    }
+
+    export default Home
