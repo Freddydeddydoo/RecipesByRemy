60389a43e0422e4828fc2bacfbc85e47968c52c8
Finn Vamosi <fgvamosi2@gmail.com>
2024-04-10 07:49:01 -0600

    fixed navbar

--- recipes-with-remy/src/Components/header.js
+++ recipes-with-remy/src/Components/header.js
@@ -18,8 +18,8 @@ function Header() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="justify-content-between" style={{ width: "100%" }}>
             <Nav.Link href="/efridge" style={getNavLinkStyle("/efridge")}>eFridge <div style={{fontSize: '15px'}}> Track your ingredients</div></Nav.Link>
-            <Nav.Link href="/search" style={{ fontSize: '1.5rem' }}>Search<div style={{fontSize: '15px'}}> Find new recipes</div></Nav.Link>
-            <Nav.Link href="/saved" style={{ fontSize: '1.5rem' }}>Saved<div style={{fontSize: '15px'}}> View saved recipes</div></Nav.Link>
+            <Nav.Link href="/search" style={getNavLinkStyle("/search")}>Search<div style={{fontSize: '15px'}}> Find new recipes</div></Nav.Link>
+            <Nav.Link href="/saved" style={getNavLinkStyle("/saved")}>Saved<div style={{fontSize: '15px'}}> View saved recipes</div></Nav.Link>
             <Nav.Link href="/profile" style={getNavLinkStyle("/profile")}>Account<div style={{fontSize: '15px'}}> Expiries and settings</div></Nav.Link>
 
           </Nav>
