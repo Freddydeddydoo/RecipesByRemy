197e25c5bc9338cb84ae01e1cc96ab7398b0672d
fardin.mahid <fardin.mahid@ucalgary.ca>
2024-04-10 02:38:50 -0600

    efridge kinda works woohoo

--- recipes-with-remy/src/Pages/efridge.js
+++ recipes-with-remy/src/Pages/efridge.js
@@ -1,23 +1,42 @@
 import React, { useState } from 'react';
 import './../CSSFiles/efridge.css';
 import Header from './../Components/header';
-import FoodData from '../DummyData/Foodnames';
+import Person1 from '../DummyData/Person1'; // Import Person1
 import Button from 'react-bootstrap/Button';
-
-const getFilteredItems = (query, foodNames) => {
-  if (!query) {
-    return [];
-  }
-  return foodNames.filter(name => name.toLowerCase().includes(query.toLowerCase()));
-};
+import Food from '../Components/food';
 
 const Efridge = () => {  
-  const [query, setQuery] = useState("");
-  const filteredItems = getFilteredItems(query, FoodData.FoodNames);
+  // Initial state for food items and recently added items
+  const [foodItems, setFoodItems] = useState(Person1[0].FoodData);
+  const [recentlyAdded, setRecentlyAdded] = useState([]);
 
-  const handleItemClick = (itemName) => {
-    // Implement the action you want to take when an item is clicked
-    alert(`You clicked on ${itemName}`);
+  // Function to handle adding a new food item
+  const handleAddFoodItem = () => {
+    // Create a new food item (You can modify this to add different default values)
+    const newFoodItem = {
+      Name: "New Food",
+      weight: 0,
+      expiry: null
+    };
+
+    // Update the food items array with the new food item
+    setFoodItems([...foodItems, newFoodItem]);
+
+    // Update the recently added items array
+    setRecentlyAdded([newFoodItem, ...recentlyAdded]);
+  };
+
+  // Function to render Food components
+  const renderFoodItems = (items) => {
+    return items.map((foodItem, index) => (
+      <Food
+        key={index}
+        foodName={foodItem.Name}
+        expiry={foodItem.expiry}
+        weight={foodItem.weight}
+        // Add image if needed
+      />
+    ));
   };
 
   return (
@@ -27,26 +46,24 @@ const Efridge = () => {
         <div className='LeftSide'>
           <div className='SearchBox'>
             <label style={{color: 'black', fontSize: '35px', padding: '10px'}}>Add Item</label>
-            <Button variant='success' className={"CustomGreene"} size='small' onClick={handleItemClick}>
-                    +
+            <Button variant='success' className={"CustomGreene"} size='small' onClick={handleAddFoodItem}>
+              +
             </Button>
           </div>
           <div className='RecentlyViewed'>
-          <label style={{color: 'black', fontSize: '35px', padding: '10px'}}> Recently Added </label>
+            <label style={{color: 'black', fontSize: '35px', padding: '10px'}}> Recently Added </label>
+            {/* Render recently added items */}
+            {renderFoodItems(recentlyAdded)}
           </div>
         </div>
         <div className='TheFridge'>
-            {/* make the top draggable aswell */}
-          <div className='Top' style={{color: 'black', fontSize: '35px', padding: '10px',paddingTop: '35px'}}>
+          {/* make the top draggable as well */}
+          <div className='Top' style={{color: 'black', fontSize: '35px', padding: '10px', paddingTop: '35px'}}>
             Your eFridge
           </div>
           <div className='Bottom'>
-            <div>bottom</div>
-            <div>bottom</div>
-            <div>bottom</div>
-            <div>bottom</div>
-            <div>bottom</div>
-            <div>bottom</div>
+            {/* Render Food components for each item in filteredItems */}
+            {renderFoodItems(foodItems)}
           </div>
         </div>
       </div>
