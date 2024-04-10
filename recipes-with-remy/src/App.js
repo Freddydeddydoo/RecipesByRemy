ba695cb4e2a0e0e618fb858f81e99b1311bf96e2
fardin.mahid <fardin.mahid@ucalgary.ca>
2024-04-10 07:12:17 -0600

    fixed some silly things

--- recipes-with-remy/src/App.js
+++ recipes-with-remy/src/App.js
@@ -3,21 +3,16 @@ import logo from './logo.svg';
 import './CSSFiles/App.css';
 import './CSSFiles/Saved.css';
 import Home from './Pages/home'
-import Login from './Pages/login'
-import signup from './Pages/signup'
+import Signup from './Pages/signup'
 import Saved from './Pages/saved'
 import Search from './Pages/search'
-import {useState, useEffect} from 'react'
 import Test from './Pages/test'
 import Test2 from './Pages/test2'
-import View from './Pages/view'
 import EditView from './Pages/editview'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './Pages/home'
 import Login from './Pages/login'
 import Profile from './Pages/profile'
 import View from './Pages/view'
-import Signup from './Pages/signup'
 import Efridge from './Pages/efridge';
 import { useState, useEffect } from 'react'
 
@@ -32,7 +27,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />} />
           <Route path="/login" element={<Login/>} />
-          <Route path="/signup" element={<signup/>} />
+          <Route path="/signup" element={<Signup/>} />
           <Route path="/saved" element={<Saved/>} />
           <Route path="/search" element={<Search/>} />
           <Route path="/test" element={<Test/>} />
@@ -41,7 +36,6 @@ function App() {
           <Route path="/editview" element={<EditView/>} />
           <Route path="/login" element={<Login />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/Signup" element={<Signup />} />
           <Route path="/efridge" element={<Efridge />} />
           <Route path="/view" element={<View />} />
           <Route path="/profile" element={<Profile />} />
